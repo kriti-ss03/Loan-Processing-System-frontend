abc93c58f6dad9ac9114043e26ae13aad6ee5729
Abhay <abhyas.work@gmail.com>
2024-07-25 12:05:29 +0530

    submit and cancel button functinality added to submitloan page

---
+++ loan-processing-system/src/app/submitloan/submit-success/submit-success.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-success',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-success.component.html',
+  styleUrl: './submit-success.component.scss'
+})
+export class SubmitSuccessComponent {
+
+}
--- loan-processing-system/src/app/submitloan/submitloan.component.ts
+++ loan-processing-system/src/app/submitloan/submitloan.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SubmitSuccessComponent } from './submit-success/submit-success.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-submitloan',
   standalone: true,
-  imports: [],
+  imports: [SubmitSuccessComponent, RouterLink],
   templateUrl: './submitloan.component.html',
   styleUrl: './submitloan.component.scss'
 })
 export class SubmitloanComponent {
+  checkSubmit= false;
 
+
+  OnSubmit(){
+    console.log("the button is clicked")
+    this.checkSubmit=true;
+  }
 }
