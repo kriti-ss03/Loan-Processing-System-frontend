66295d43aec134650466c220a39546f47a646172
Priyanshi Nama <priyanshinama@fico.com>
2024-07-28 23:42:59 +0530

    working, folks

---
+++ src/app/view-application/view-application.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ViewApplicationService {
+//   private apiUrl = 'https://api.example.com/loan-application'; // Replace with your API URL
+
+//   constructor(private http: HttpClient) {}
+
+//   getApplicationData(): Observable<any> {
+//     return this.http.get<any>(this.apiUrl);
+//   }
+// }
---
+++ src/app/viewapps/viewapps.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Application {
+  applicationId: number;
+  firstName: string;
+  middleName:string,
+  lastName:string,
+  submittedDate: Date;
+  status: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewappsService {
+  private apiUrl = 'http://localhost:8080/phansbank/v1/viewapps'; // Replace with your actual backend URL
+
+  constructor(private http: HttpClient) { }
+
+  getApplications(): Observable<Application[]> {
+    return this.http.get<Application[]>(this.apiUrl);
+  }
+  getApplicationById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+}
