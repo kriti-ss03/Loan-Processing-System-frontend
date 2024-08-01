b1981a8bc5efa806c89220b17a47b65f275f6d95
Abhay <abhyas.work@gmail.com>
2024-08-01 18:29:58 +0530

    unwanted code removed

--- loan-processing-system/src/app/submitloan/submitloan.component.ts
+++ loan-processing-system/src/app/submitloan/submitloan.component.ts
@@ -3,21 +3,8 @@ import { SubmitSuccessComponent } from './submit-success/submit-success.componen
 import { RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient} from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-// import { ApplicationData } from './submitloan.service';
 
-
-// // add
-// interface ApplicationData {
-//   // Define your data structure here
-//   firstName: string;
-//   lastName: string;
-//   middleName:string;
-//   dob:Date;
-//   email:string;
-//   // ... other fields
-// }
 
 @Component({
   selector: 'app-submitloan',
@@ -29,20 +16,10 @@ import { CommonModule } from '@angular/common';
 export class SubmitloanComponent implements OnInit {
    checkSubmit= false;
 
-  // this is of no use now, later on remove the onSubmit() function from the html file and this file as well
-  // OnSubmit(){
-    // console.log("the button is clicked")
-    // this.checkSubmit=true;
-  // }
   applicationForm!: FormGroup;
   successMessage: string | null = null;
   errorMessage: string | null = null;
   formErrors: string[] = [];
-  // declineRules: string[]= []
-  // applicantAge!:number
-  // applicantSalary!:number
-  // applicantExp!:number
-  //  applicationData!: ApplicationData
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
@@ -85,27 +62,19 @@ export class SubmitloanComponent implements OnInit {
 
   submitForm() {
     this.formErrors = [];
-    // this.declineRules=[];
+
     if (this.applicationForm.valid) {
       let applicationData= this.applicationForm.value;
-      // this.applicantSalary = applicationData.annualSalary
-      // this.applicantExp = applicationData.workExperienceMonths + 12*applicationData.workExperienceYears
-      // // change this to real time age
-      // this.applicantAge = 33
+
       console.log(applicationData)
-// <<<<<<< main
-//       this.checkSubmit=true;
-// =======
-      // if(this.applicantSalary>10000&&this.applicantExp>6&&this.applicantAge>18&&this.applicantAge<65){
         this.checkSubmit=true;
-// >>>>>>> main
-      this.http.post('http://localhost:8080/phansbank/v1/submit', applicationData) // Replace with your API endpoint
+      this.http.post('http://localhost:8080/phansbank/v1/submit', applicationData)
         .subscribe(
           (response) => {
-            // console.log('Application submitted successfully:', response);
+
             this.successMessage = 'Application submitted successfully!';
             this.errorMessage = null;
-            this.applicationForm.reset(); // Optionally reset the form
+            this.applicationForm.reset();
           },
           (error) => {
             console.error('Error submitting application:', error);
@@ -113,20 +82,6 @@ export class SubmitloanComponent implements OnInit {
             this.successMessage = null;
           }
         );
-      // }
-      // else{
-      //   if(this.applicantSalary<10000){
-      //     this.declineRules.push("The application cannot be submitted as the annual salary is less than $10,000")
-      //   }
-      //   if(this.applicantExp<6){
-      //     this.declineRules.push("The application cannot be submitted as the working exprience is less than 6 months")
-      //   }
-      //   if(this.applicantAge<18&&this.applicantAge>65){
-      //     this.declineRules.push("The application cannot be sunbmitted as the age is not in the range of 18 to 65")
-      //   }
-
-      // }
-
     }
     else {
       this.displayErrors();
