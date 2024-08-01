1246b487c7a13d75757e308e5d479f816a84a3df
Nirupma Agrahari <agraharinirupma@gmail.com>
2024-08-01 11:54:27 +0530

    fnei

--- loan-processing-system/src/app/view-application/view-application.service.ts
+++ loan-processing-system/src/app/view-application/view-application.service.ts
@@ -14,41 +14,6 @@
 //     return this.http.get<any>(this.apiUrl);
 //   }
 // }
-<<<<<<< HEAD
-export interface Application {
- 
-    id: number,
-    status: string,
-    score: number,
-    declineReason: string,
-    firstName: string,
-    middleName: string,
-    lastName: string,
-    dateOfBirth: Date,
-    maritalStatus: string,
-    ssnNumber: number,
-    loanAmount: number,
-    loanPurpose: number,
-    description: string,
-    addressLine1: string,
-    addressLine2: string,
-    city: string,
-    state: string,
-    postalCode: number,
-    phoneHome: number,
-    phoneOffice: number,
-    phoneMobile: number,
-    emailAddress: string,
-    employerName: string,
-    annualSalary: number,
-    workExperienceYears: number,
-    workExperienceMonths:number,
-    designation: string,
-    employerAddressLine1:string,
-    employerAddressLine2:string,
-    employerCity:string,
-    employerState:string
-=======
 
 export interface Application {
 
@@ -83,5 +48,4 @@ export interface Application {
       employerAddressLine2:string,
       employerCity:string,
       employerState:string
->>>>>>> b41557f1cbd4c5a7ee5e2ad46754ac3c1c72c75b
 }
