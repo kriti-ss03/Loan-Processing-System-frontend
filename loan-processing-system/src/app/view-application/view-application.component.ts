302b87cdcd65b968287e13f314c4fd88118c5098
Abhay <abhyas.work@gmail.com>
2024-08-01 21:09:39 +0530

    decline reason logic updated

--- loan-processing-system/src/app/view-application/view-application.component.ts
+++ loan-processing-system/src/app/view-application/view-application.component.ts
@@ -18,19 +18,20 @@ export class ViewApplicationComponent implements OnInit {
   applicantSalary!:number
   applicantExp!:number
 
-  calculateAge(dofb:string, submittedDate:string ){
-    let dob = new Date(dofb);
-    let currentDate = new Date(submittedDate);
 
-    let age = currentDate.getFullYear() - dob.getFullYear();
+  // calculateAge(dofb:string, submittedDate:string ){
+  //   let dob = new Date(dofb);
+  //   let currentDate = new Date(submittedDate);
 
-    if (currentDate.getMonth() < dob.getMonth() ||
-        (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
-        age--;
-    }
+  //   let age = currentDate.getFullYear() - dob.getFullYear();
 
-    return age;
-  }
+  //   if (currentDate.getMonth() < dob.getMonth() ||
+  //       (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
+  //       age--;
+  //   }
+
+  //   return age;
+  // }
   // @Input({required:true}) userId!:number
   // i have a option of changing thw way how i take the data from backend and display on client side
   // currently i am going witht the form method but if this dosent work, then i can make the interface ofn application form and take the values similar to the one i did in viewapps code
@@ -52,18 +53,19 @@ export class ViewApplicationComponent implements OnInit {
       this.viewappsService.getApplicationById(Number(id)).subscribe(data => {
         this.application = data
 
-        this.applicantSalary = this.application.annualSalary ;
-        this.applicantExp = this.application.workExperienceYears*12 + this.application.workExperienceMonths;
-        this.applicantAge =  this.calculateAge(this.application.dateOfBirth, this.application.submittedDate)
-        if(this.applicantSalary<10000){
-          this.declineRules.push("Annual salary is less than $10,000")
-        }
-        if(this.applicantExp<6){
-          this.declineRules.push("Working exprience is less than 6 months")
-        }
-        if(this.applicantAge<18&&this.applicantAge>65){
-          this.declineRules.push("Age is not in the range of 18 to 65")
-        }
+        this.declineRules = this.application.declineReason.split('.');
+        // this.applicantSalary = this.application.annualSalary ;
+        // this.applicantExp = this.application.workExperienceYears*12 + this.application.workExperienceMonths;
+        // this.applicantAge =  this.calculateAge(this.application.dateOfBirth, this.application.submittedDate)
+        // if(this.applicantSalary<10000){
+        //   this.declineRules.push("Annual salary is less than $10,000")
+        // }
+        // if(this.applicantExp<6){
+        //   this.declineRules.push("Working exprience is less than 6 months")
+        // }
+        // if(this.applicantAge<18&&this.applicantAge>65){
+        //   this.declineRules.push("Age is not in the range of 18 to 65")
+        // }
       });
 
   }
